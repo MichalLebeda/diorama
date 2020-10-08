1475abed84d0a18e86e2f58841b7dbab365b595e
Michal Lebeda <MichalLebeda@users.noreply.github.com>
2020-10-08 23:08:45 +0200

    Android: request permission

--- android/src/cz/shroomware/diorama/AndroidLauncher.java
+++ android/src/cz/shroomware/diorama/AndroidLauncher.java
@@ -1,5 +1,8 @@
 package cz.shroomware.diorama;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
@@ -8,8 +11,16 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import cz.shroomware.diorama.engine.EngineGame;
 
 public class AndroidLauncher extends AndroidApplication {
+	private static final int WRITE_REQUEST_CODE = 2;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+			if (this.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+				this.requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_REQUEST_CODE);
+			}
+		}
+
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new EngineGame(), config);
